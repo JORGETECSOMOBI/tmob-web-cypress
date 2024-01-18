913efe5733791a887e4617af42826018f5ffd358
unknown <jorge.luiz@tecsomobi.com.br>
2024-01-18 16:34:48 -0300

    [tarifário]Inclusão de testes arquivos operacionais

--- cypress/pages/Tarifario/ArquivosOperacionaisPage.js
+++ cypress/pages/Tarifario/ArquivosOperacionaisPage.js
@@ -16,5 +16,51 @@ class ArquivosOperacionaisPage {
             .clicaBotao('Arquivos Operacionais')
         return this
     }
+
+    filtroPorId() {
+        componente
+            .escreveId('912')
+            .validaPesquisaAntTableRow('912')
+        return this
+    }
+    
+    filtroPorNomeDoarquivo() {
+        componente
+            .escreveId('1704924401635_op.enc')
+            .validaPesquisaAntTableRow('1704924401635_op.enc')
+        return this
+    }
+
+    filtrandoPorTagAtual() {
+        this
+        .filtroPorStatus('Atual')
+        .validaTag('Atual')
+        return this
+    }
+
+    filtrandoPorTagPrincipal() {
+        this
+        .filtroPorStatus('Principal')
+        .validaTag('Principal')
+        return this
+    }
+
+    filtrandoPorTagAlt() {
+        this
+        .filtroPorStatus('Alt')
+        .validaTag('Alt')
+        return this
+    }
+
+    filtroPorStatus(tag) {
+        cy.get('#status').click({ force: true })
+        cy.get('[title]').contains(tag).click({ force: true })
+        return this
+    }
+
+    validaTag(tag) {
+        cy.get(':nth-child(6) > .ant-tag').should('contain', tag)
+        return this
+    }
 }
 export default new ArquivosOperacionaisPage
