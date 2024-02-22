b134119ee68ee363df9577d69545946614126d3e
Jorge Luiz Cardoso <jorge.luiz@tecsomobi.com.br>
2024-02-22 13:56:09 -0300

    Atualização

--- cypress/e2e/Operacional/Comercializacao/ArquivosOperacionais/Testando-arquivos-operacionais.cy.js
+++ cypress/e2e/Operacional/Comercializacao/ArquivosOperacionais/Testando-arquivos-operacionais.cy.js
@@ -1,36 +1,36 @@
 import arquivos from '../../../../pages/Comercializacao/ArquivosOperacionaisPage'
 
-describe('Testando tela "Dashboard"', () => {
+describe('Testando tela "Arquivos operacionais"', () => {
 
     beforeEach(() => {
         arquivos.beforeArquivosOperacionais()
     })
 
     it('Filtrando por "ID"', () => {
-        
+        arquivos.filtroPoId()
     })
 
     it('Filtrando por "Nome do arquivo"', () => {
-        
+        arquivos.filtroPoNomeArquivo()
     })
 
     it('Filtrando por tag "Atual"', () => {
-        
+        arquivos.filtroPoTagAtual()
     })
 
     it('Filtrando por tag "Principal"', () => {
-        
+        arquivos.filtroPoTagPrincipal()
     })
 
     it('Filtrando por tag "Alt"', () => {
-        
+        arquivos.filtroPoTagAlt()
     })
 
     it('Testando função "Forçar geração"', () => {
-        
+        arquivos.forcarGeracao()
     })
 
     it('Testando função "Limpar filtro"', () => {
-        
+        arquivos.limpaFiltro()
     })
 })
--- cypress/pages/Comercializacao/ArquivosOperacionaisPage.js
+++ cypress/pages/Comercializacao/ArquivosOperacionaisPage.js
@@ -12,5 +12,57 @@ class ArquivosOperacionaisPage {
             .clicaBotao('Arquivos Operacionais')
             .selecionaIdioma('PT')
     }
+
+    filtroPoId() {
+        componente
+            .pesquisaId('244')
+            .validaPesquisaAntTableCell('244')
+        return this
+    }
+
+    filtroPoNomeArquivo() {
+        componente
+            .pesquisaId('1707232227015_op.enc')
+            .validaPesquisaTitle('1707232227015_op.enc')
+        return this
+    }
+
+    filtroPoTagAtual() {
+        cy.get('#status').click({ force: true })
+        cy.contains('Atual').click({ force: true })
+        componente
+            .validaPesquisaTitle('Atual')
+        return this
+    }
+
+    filtroPoTagPrincipal() {
+        cy.get('#status').click({ force: true })
+        cy.contains('Principal').click({ force: true })
+        componente
+            .validaPesquisaTitle('Principal')
+        return this
+    }
+
+    filtroPoTagAlt() {
+        cy.get('#status').click({ force: true })
+        cy.contains('Alt').click({ force: true })
+        componente
+            .validaPesquisaTitle('Alt')
+        return this
+    }
+
+    forcarGeracao() {
+        componente
+            .clicaBotao('Forçar geração')
+            .clicaBotaoModal('Sim')
+        return this
+    }
+
+    limpaFiltro() {
+        componente
+            .escreveId('123123')
+            .limpaFiltro()
+        return this
+    }
 }
 export default new ArquivosOperacionaisPage
